1e0e186be0da2f14c2be0a207d97a47557f87039
hxzd5568 <3257591325@qq.com>
2025-07-17 14:37:13 +0800

    [New] save meta info

--- extract_cv_unittest.py
+++ extract_cv_unittest.py
@@ -1,7 +1,11 @@
+import os
+import json
 import torch, torchvision
 import numpy as np
 from torchvision import transforms
 from torch.export import export
+import utils
+from torch import nn
 from utils import convert_param_name, indent_with_tab, apply_templates
 
 normalize = transforms.Normalize(
@@ -9,8 +13,8 @@ normalize = transforms.Normalize(
     std=[0.229, 0.224, 0.225]
 )
 
-batch_size = 2
-height, width = 224, 220
+batch_size = 1
+height, width = 1, 5
 num_channels = 3
 
 random_input = torch.rand(batch_size, num_channels, height, width)
@@ -38,14 +42,18 @@ params = exported.state_dict
 new_params = dict()
 for k, v in params.items():
     new_params[convert_param_name(k)] = v
-torch.save(new_params, "model_weights.pth")
-
-inputs = exported.example_inputs[0][0]
-torch.save(inputs, "inputs.pt")
 
 base_code = exported.graph_module.__str__()
-write_code = indent_with_tab(base_code)
-write_code = apply_templates(write_code)
-with open(f'test_export_{model_name}.py', 'w') as fp:
+write_code = apply_templates(base_code)
+if not os.path.exists(f"{model_name}/"):
+    os.mkdir(f"{model_name}")
+with open(f'{model_name}/model.py', 'w') as fp:
     print(write_code, file=fp)
-print(model(inputs))
+data = {"framework": "torch"}
+file_path = f'{model_name}/attribute.json'
+with open(file_path, 'w') as f:
+    json.dump(data, f, indent=4)
+
+converted = utils.convert_state_and_inputs(params, exported.example_inputs[0])
+utils.save_converted_to_text(converted, file_path=f'{model_name}/source_tensor_meta.py')
+utils.save_constraints_text(converted, file_path=f'{model_name}/input_tensor_constraints.py')
